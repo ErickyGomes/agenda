1b0d53c8f458831b2d94c630ffbb3de0c16c2fee
ErickyGomes <44137511+ErickyGomes@users.noreply.github.com>
2023-02-22 14:52:46 -0300

    update data
    remoção da senha

--- agenda/settings.py
+++ agenda/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yi-$e-_^8s)=8in9z^7k&(&l34i0g)-#$ha)(t#a*(qq0*j8o5'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
